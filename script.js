5d4d91266ad0559bc35190b95419b94d36e7062f
AlexCao-html <caobubo@gmail.com>
2021-04-30 22:14:54 -0700

    fixed errors

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ document.querySelector(".X").addEventListener("click", () => {
     document.querySelector("nav").classList.remove("toggled");
     document.querySelector(".X").classList.remove("toggled");
 });
+
+x = document.querySelectorAll("nav a");
+for (let index = 0; index < x.length; index++) {
+    const i = x[index];
+    i.addEventListener("click", () => {
+        document.querySelector("nav").classList.remove("toggled");
+        document.querySelector(".X").classList.remove("toggled");    
+    });    
+}
